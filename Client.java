7ae5aa03d86e94cbded5fc689fb4260c224da2fe
SercanOner <sercan75@hotmail.fr>
2017-06-23 11:13:30 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,26 +2,96 @@ package JPAClass;
 
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "titre", discriminatorType = DiscriminatorType.STRING, length = 10)
-public class abstract Client {
+public abstract class Client {
 		
-		private long id_Client;
+	
+		@EmbeddedId
+		private Adresse id;
+	
+		private Long id_Client;
 		private String nom;
 		private int  numeroTel;
 		private int numeroFax;
 		private String email;
 		
+		
+		
+		@OneToMany(mappedBy="client")
+		private Reservation reservation;
+		
+		
+		
+		@OneToOne
+		@JoinColumn(name="id_Login")
+		private Login login;
+		
+		
 		public Client()
 		{
 			
 		}
-		public long getId_Client() {
+		
+		
+		
+		
+
+
+
+		
+
+
+		public Client(Adresse id, Long id_Client, String nom, int numeroTel, int numeroFax, String email,
+				Reservation reservation, Login login) {
+			super();
+			this.id = id;
+			this.id_Client = id_Client;
+			this.nom = nom;
+			this.numeroTel = numeroTel;
+			this.numeroFax = numeroFax;
+			this.email = email;
+			this.reservation = reservation;
+			this.login = login;
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+		public Adresse getId() {
+			return id;
+		}
+
+
+
+
+
+		public void setId(Adresse id) {
+			this.id = id;
+		}
+
+
+
+
+
+		public Long getId_Client() {
 			return id_Client;
 		}
 		public void setId_Client(long id_Client) {
@@ -51,6 +121,60 @@ public class abstract Client {
 		public void setEmail(String email) {
 			this.email = email;
 		}
+
+
+
+
+
+
+
+
+
+
+		public Reservation getReservation() {
+			return reservation;
+		}
+
+
+
+
+
+
+
+
+
+
+		public void setReservation(Reservation reservation) {
+			this.reservation = reservation;
+		}
+
+
+
+
+
+
+
+
+
+
+		public Login getLogin() {
+			return login;
+		}
+
+
+
+
+
+
+
+
+
+
+		public void setLogin(Login login) {
+			this.login = login;
+		}
+		
+		
 		
 		
 }
