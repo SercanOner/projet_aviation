f802e443d4046737cc16e21b4adff99604138e29
SercanOner <sercan75@hotmail.fr>
2017-06-23 11:25:41 +0200

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -2,21 +2,60 @@ package JPAClass;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+@Entity
 public class Reservation {
 	
-		private long id_Reservation;
+		private Long id_Reservation;
 		private Date date;
 		private int numero;
+		
+		@ManyToOne
+		@JoinColumn(name="id_Passager")
+		private Passager passager;
+		
+		
+		@ManyToOne
+		@JoinColumn(name="id_Client")
+		private Client client;
+		
+		
+		
+		@ManyToOne
+		@JoinColumn(name="id_Vol")
+		private Vol vol;
+		
 		
 		public Reservation(){
 			
 		}
+		
+		
+		
+		
 
-		public long getId_Reservation() {
+		public Reservation(Long id_Reservation, Date date, int numero, Passager passager, Client client) {
+			super();
+			this.id_Reservation = id_Reservation;
+			this.date = date;
+			this.numero = numero;
+			this.passager = passager;
+			this.client = client;
+		}
+
+
+
+
+
+		public Long getId_Reservation() {
 			return id_Reservation;
 		}
 
-		public void setId_Reservation(long id_Reservation) {
+		public void setId_Reservation(Long id_Reservation) {
 			this.id_Reservation = id_Reservation;
 		}
 
@@ -35,5 +74,40 @@ public class Reservation {
 		public void setNumero(int numero) {
 			this.numero = numero;
 		}
+
+
+
+
+
+		public Passager getPassager() {
+			return passager;
+		}
+
+
+
+
+
+		public void setPassager(Passager passager) {
+			this.passager = passager;
+		}
+
+
+
+
+
+		public Client getClient() {
+			return client;
+		}
+
+
+
+
+
+		public void setClient(Client client) {
+			this.client = client;
+		}
+		
+		
+		
 		
 }
