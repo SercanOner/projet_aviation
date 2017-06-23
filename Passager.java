cd3b8b597bd98391ac3791546cae25e764f4710d
SercanOner <sercan75@hotmail.fr>
2017-06-23 11:09:21 +0200

    Update Passager.java

--- Passager.java
+++ Passager.java
@@ -1,13 +1,55 @@
+
 package JPAClass;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
 public class Passager {
+	
+	
+	@EmbeddedId
+	private Adresse id;
+	
 	private long id_Passager;
 	private String nom;
 	private String prenom;
 	
+	@OneToMany(mappedBy="passager")
+	private Reservation reservation;
+	
+	
+	
+	
+	
+
+	
 	public Passager(){
 		
 	}
+	
+	
+	
+
+
+
+
+
+	public Passager(Adresse id, long id_Passager, String nom, String prenom, Reservation reservation) {
+		super();
+		this.id = id;
+		this.id_Passager = id_Passager;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.reservation = reservation;
+	}
+
+
+
+
+
+
+
 
 	public long getId_Passager() {
 		return id_Passager;
@@ -32,6 +74,45 @@ public class Passager {
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+
+
+
+
+	public Reservation getReservation() {
+		return reservation;
+	}
+
+
+
+
+	public void setReservation(Reservation reservation) {
+		this.reservation = reservation;
+	}
+
+
+
+
+
+
+
+
+	public Adresse getId() {
+		return id;
+	}
+
+
+
+
+
+
+
+
+	public void setId(Adresse id) {
+		this.id = id;
+	}
+	
+	
+	
 	
 	
 	
