4d29f5849ef4552ba10c3f4fcb4b140145761e30
Mizuro <salomon.kwimang@gmail.com>
2017-06-23 12:10:06 +0200

    Update CompagnieAerienne.java

--- CompagnieAerienne.java
+++ CompagnieAerienne.java
@@ -1,9 +1,24 @@
 package JPAClass;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class CompagnieAerienne {
 	
-	private long id_CompagnieAerienne;
+	@Id 
+	@GeneratedValue
+	private Long id_CompagnieAerienne;
 	private String nom;
+	
+	@OneToMany(mappedBy="compagnieAerienne")
+	private List<CompagnieAerienneVol> compagnieAerienneVol = new ArrayList<>();
+	
 	public CompagnieAerienne(){
 		
 	}
@@ -19,6 +34,17 @@ public class CompagnieAerienne {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	public List<CompagnieAerienneVol> getCompagnieAerienneVol() {
+		return compagnieAerienneVol;
+	}
+	public void setCompagnieAerienneVol(List<CompagnieAerienneVol> compagnieAerienneVol) {
+		this.compagnieAerienneVol = compagnieAerienneVol;
+	}
+	public void setId_CompagnieAerienne(Long id_CompagnieAerienne) {
+		this.id_CompagnieAerienne = id_CompagnieAerienne;
+	}
+	
 	
 
 }
+
