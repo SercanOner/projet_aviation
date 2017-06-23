d1556bd8f6bd77f0cd0521de09ae7663c337ea3c
Mizuro <salomon.kwimang@gmail.com>
2017-06-23 12:11:30 +0200

    Update CompagnieAerienneVol.java

--- CompagnieAerienneVol.java
+++ CompagnieAerienneVol.java
@@ -1,20 +1,57 @@
 package JPAClass;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class CompagnieAerienneVol {
 	
-		private String numero;
-		
-		public CompagnieAerienneVol(){
-			
-		}
+	@Id
+	@GeneratedValue
+	private Long id_CompagnieAerienneVol;
+	
+	private String numero;
+	
+	@ManyToOne
+	@JoinColumn(name="CompagnieAerienne_ID")
+	private CompagnieAerienne compagnieAerienne;
 
-		public String getNumero() {
-			return numero;
-		}
+	@ManyToOne
+	@JoinColumn(name="Vol_ID")
+	private Vol vol;
+	
+	public CompagnieAerienneVol(){
+		
+	}
 
-		public void setNumero(String numero) {
-			this.numero = numero;
-		}
-		
-		
+	
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public CompagnieAerienne getCompagnieAerienne() {
+		return compagnieAerienne;
+	}
+
+
+	public void setCompagnieAerienne(CompagnieAerienne compagnieAerienne) {
+		this.compagnieAerienne = compagnieAerienne;
+	}
+
+
+	public Vol getVol() {
+		return vol;
+	}
+
+
+	public void setVol(Vol vol) {
+		this.vol = vol;
+	}
 }
