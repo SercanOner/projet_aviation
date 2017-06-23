1b98f17256eeffd7794e02ac05714956993b1af4
SercanOner <sercan75@hotmail.fr>
2017-06-23 11:07:57 +0200

    Update Adresse.java

--- Adresse.java
+++ Adresse.java
@@ -1,20 +1,25 @@
 package JPAClass;
 
+import java.io.Serializable;
+
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
+
 
 @Embeddable
-public class Adresse implements Serializable{
+public class Adresse implements Serializable  {
 
+	
 	private String adresse;
 	private String codePostal;
 	private String ville;
 	private String pays;
 	
+	
+	
 	public Adresse() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-
 	public Adresse(String adresse, String codePostal, String ville, String pays) {
 		super();
 		this.adresse = adresse;
@@ -22,7 +27,6 @@ public class Adresse implements Serializable{
 		this.ville = ville;
 		this.pays = pays;
 	}
-	
 	public String getAdresse() {
 		return adresse;
 	}
@@ -47,6 +51,52 @@ public class Adresse implements Serializable{
 	public void setPays(String pays) {
 		this.pays = pays;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
+		result = prime * result + ((codePostal == null) ? 0 : codePostal.hashCode());
+		result = prime * result + ((pays == null) ? 0 : pays.hashCode());
+		result = prime * result + ((ville == null) ? 0 : ville.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Adresse other = (Adresse) obj;
+		if (adresse == null) {
+			if (other.adresse != null)
+				return false;
+		} else if (!adresse.equals(other.adresse))
+			return false;
+		if (codePostal == null) {
+			if (other.codePostal != null)
+				return false;
+		} else if (!codePostal.equals(other.codePostal))
+			return false;
+		if (pays == null) {
+			if (other.pays != null)
+				return false;
+		} else if (!pays.equals(other.pays))
+			return false;
+		if (ville == null) {
+			if (other.ville != null)
+				return false;
+		} else if (!ville.equals(other.ville))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+	
 	
 	
 }
